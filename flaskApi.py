bf5b3eba5e70f4b02fa6b53927f7f56773a0da57
rahulsisupalan <rahulsisupalan1998@gmail.com>
2026-01-04 21:26:11 +0530

    flaskapi code updated

--- flaskApi.py
+++ flaskApi.py
@@ -3,17 +3,21 @@ import os
 import joblib
 import numpy as np
 import logging
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token,current_user,jwt_required,get_jwt_identity
+
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s"
 )
 
-modelfolder = "app/model"
+modelfolder = "R:/mlops/model"
 modelname = "winemodel"
 version = "v1" 
 
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"]="FVKEFNVKLEFLVJLFELVOEJVOEI4R802874029RJKFNRKJFR"
+jwt=JWTManager(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))   # /app/api
 modelPath = os.path.join(BASE_DIR, "..", modelfolder, f"{modelname}_{version}.pkl")
@@ -39,12 +43,31 @@ def input_check(inputdata):
         inputarray.append(value)
     return np.array(inputarray)
 
+
+@app.route("/login",methods=["POST"])
+def login():
+    data=dict(request.get_json())
+    username=data.get("username")
+    password=data.get("password")
+    print("__________________________________")
+
+    if not username or not password:
+        return jsonify({"success": False, "message": "Invaild username or password"})
+    
+    access_token=create_access_token(identity=username)
+    return jsonify({"success": True, "message": access_token})
+    
+
+
+
 @app.route("/", methods=["GET"])
 def health_check():
     return jsonify({"success": True, "message": "Server is Running"})
 
 @app.route("/predict", methods=["POST"])
+@jwt_required()
 def model_predict():
+    current_user = get_jwt_identity()
     data = request.get_json()
     if not data:
         return jsonify({"success": False, "message": "No Input Data"})
